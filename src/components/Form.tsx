7fa1875dd8c32a41444507f93c5867ce1da81f7a
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-11 09:51:38 -0600

    modificando formulario para el envio del mismo

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 import {categories} from "../data/categories"
-import { useState, ChangeEvent, act } from "react"
+import { useState, ChangeEvent, FormEvent } from "react"
 import { Activity } from "../types"
 
 export default function Form() {
@@ -11,21 +11,25 @@ export default function Form() {
   })
   
 
-  const handleChange = (e: ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     const isNumberField = ['category', 'calories'].includes(e.target.id)
     setActivity({
         ...activity,
         [e.target.id]: isNumberField ? +e.target.value : e.target.value
     })
   }
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) =>{
+    e.preventDefault()
+    console.log('submit...')
+  }
 
-  const isValidActivity = () =>{
+  const isValidActivity = () => {
     const {name, calories} = activity
     return name.trim() !== '' && calories > 0
   }
 
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg" onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categoría:</label>
             <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" name="category" id="category" value={activity.category} onChange={handleChange}>
@@ -44,7 +48,7 @@ export default function Form() {
             <label htmlFor="calories" className="font-bold">Calorias:</label>
             <input id="calories" type="number" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. 300 o 500" value={activity.calories} onChange={handleChange}/>
         </div>
-        <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold, uppercase text-white disabled:opacity-10" value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'} disabled={!isValidActivity()} />
+        <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white disabled:opacity-10" value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'} disabled={!isValidActivity()} />
     </form>
 
   )
